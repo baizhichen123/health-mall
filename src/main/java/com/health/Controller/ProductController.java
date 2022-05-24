971b63f7fe71333232e0fd7d7811282930244629
baizhichen123 <34640143+baizhichen123@users.noreply.github.com>
2022-05-24 11:23:45 +0800

    #修改logger日志

--- src/main/java/com/health/Controller/ProductController.java
+++ src/main/java/com/health/Controller/ProductController.java
@@ -2,22 +2,20 @@ package com.health.Controller;
 
 import com.health.Model.Product;
 import com.health.Service.ProductService;
+import com.health.Service.RedisService;
 import com.health.util.CommResult;
 import com.health.util.CommonPage;
-import com.log.starter.annotation.PrintLog;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/product")
@@ -30,22 +28,25 @@ public class ProductController {
     private ProductService productService;
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisService redisService;
 
     @RequestMapping("/listProduct")
     @ResponseBody
-    @ApiOperation("分页获取所有商品")
+    @ApiOperation("分页获取所有商品") 
     public CommResult<CommonPage> listAllProduct(){
 
         logger.info("分页获取所有商品");
 
-        List<Product> list=productService.listAllProduct();
 
-        System.out.println(list.size());
+        Object productList = redisService.get("productList");
 
-        //redisTemplate.opsForValue().set("productList",list);
-
-    return CommResult.success(CommonPage.restPage(list));
+        if(Objects.isNull(productList)){
+            List<Product> list=productService.listAllProduct();
+            redisService.set("productList",list);
+            return CommResult.success(CommonPage.restPage((List)list));
+        }else {
+            return CommResult.success(CommonPage.restPage((List)productList));
+        }
 
     }
 
@@ -62,9 +63,9 @@ public class ProductController {
     @ResponseBody
     public String getProduct(){
 
-        String  userName = (String) redisTemplate.opsForValue().get("productList");
+        //String  userName = (String) redisTemplate.opsForValue().get("productList");
 
-        return userName;
+        return "";
 
     }
 
